205d63e6a92dd5b44dd2804e2b85a9f428e40cfe
smithak15 <smithak15@gmail.com>
2016-06-03 01:38:54 -0400

    Initial commit

---
+++ src/com/helloword/controller/ControllerServlet.java
@@ -0,0 +1,57 @@
+package com.helloword.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerServlet
+ */
+@WebServlet("/ControllerServlet")
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControllerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request,response);
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		doProcess(request,response);
+	}
+	
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String requestUri = request.getRequestURI();
+		if(requestUri.equalsIgnoreCase("/HelloWorldAssignment/registrationSubmit.do")){
+			registerUser(request,response);
+		}
+	}
+
+	private void registerUser(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
